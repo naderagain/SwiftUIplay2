43f7186537d7ce06443c74d10d3dd5089f36d508
Nader Ahmed <nader.ahmed@hh2.com>
2024-06-23 18:31:34 -0400

    Initial Commit

---
+++ SwiftUIplay2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIplay2
+//
+//  Created by Nader Ahmed on 6/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIplay2/SwiftUIplay2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIplay2App.swift
+//  SwiftUIplay2
+//
+//  Created by Nader Ahmed on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIplay2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
